42b7ef94041236e7ae2f143cb54a8cf39eb65741
Muhammad Athallah <mhd.athallah@gmail.com>
2023-03-11 22:24:03 +0700

    add: view implementation

--- main/urls.py
+++ main/urls.py
@@ -8,8 +8,6 @@ urlpatterns = [
     path('cari/panjang/', search_length_page, name='search_length'),
     path('cari/prefiks/', search_prefix_page, name='search_prefix'),
     path('cari/sufiks/', search_suffix_page, name='search_suffix'),
-    path('cari/panjang/<int:length>', show_json_by_length, name='show_json_by_length'),
-    path('cari/prefiks/<str:letter>', show_json_by_start, name='show_json_by_start'),
-    path('cari/sufiks/<str:letter>', show_json_by_end, name='show_json_by_end'),
     path('cari/', search_words, name='search_words'),
+    path('definisi/<str:word>', show_definition, name='show_definition'),
 ]
--- main/views.py
+++ main/views.py
@@ -34,32 +34,50 @@ def homepage(request):
     return render(request, "homepage.html")
 
 def search_length_page(request):
+    if request.method == 'POST':
+        print(request.POST.get('panjang'))
+        length = int(request.POST.get('panjang'))
+        max_length = None
+        if request.POST.get('batas'):
+            max_length = int(request.POST.get('batas'))
+        return search_length_function(request, length, max_length)
+    
     return render(request, "search_length.html")
 
 def search_prefix_page(request):
+    if request.method == 'POST':
+        prefix = str(request.POST.get('prefiks'))
+        return search_prefix_function(request, prefix)
+
     return render(request, "search_prefix.html")
 
 def search_suffix_page(request):
+    if request.method == 'POST':
+        suffix = str(request.POST.get('sufiks'))
+        return search_suffix_function(request, suffix)
+    
     return render(request, "search_suffix.html")
 
-def show_all_json(request):
-    data = Kata.objects.all()
-    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+def show_definition(request, word):
+    return render(request, "definition.html")
 
-def show_json_by_length(request, length):
+def search_length_function(request, length, max_length):
     words = Kata.objects.all()
-    response = search_by_length(words, length)
-    return HttpResponse(serializers.serialize("json", response), content_type="application/json")
+    response = search_by_length(words, length, max_length)
+    context = {'words' : response}
+    return render(request, "result.html", context)
 
-def show_json_by_start(request, prefix):
+def search_prefix_function(request, prefix):
     words = Kata.objects.all()
     response = search_by_prefix(words, prefix)
-    return HttpResponse(serializers.serialize("json", response), content_type="application/json")
+    context = {'words' : response}
+    return render(request, "result.html", context)
 
-def show_json_by_end(request, suffix):
+def search_suffix_function(request, suffix):
     words = Kata.objects.all()
     response = search_by_suffix(words, suffix)
-    return HttpResponse(serializers.serialize("json", response), content_type="application/json")
+    context = {'words' : response}
+    return render(request, "result.html", context)
 
 def search_words(request):
     # Example usage: http://localhost:8000/cari/?panjang=5&batas=10&prefiks=an&sufiks=i
@@ -86,4 +104,23 @@ def search_words(request):
         else:
             response = search_by_suffix(response, suffix)
     
+    return HttpResponse(serializers.serialize("json", response), content_type="application/json")
+
+def show_all_json(request):
+    data = Kata.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_length(request, length):
+    words = Kata.objects.all()
+    response = search_by_length(words, length)
+    return HttpResponse(serializers.serialize("json", response), content_type="application/json")
+
+def show_json_by_start(request, prefix):
+    words = Kata.objects.all()
+    response = search_by_prefix(words, prefix)
+    return HttpResponse(serializers.serialize("json", response), content_type="application/json")
+
+def show_json_by_end(request, suffix):
+    words = Kata.objects.all()
+    response = search_by_suffix(words, suffix)
     return HttpResponse(serializers.serialize("json", response), content_type="application/json")
